6669bc6ec3e38ad955cf27882edf1c69201f8556
yogendra-17 <f20190287@goa.bits-pilani.ac.in>
2022-08-12 10:45:51 +0530

    asd

--- passport-config.js
+++ passport-config.js
@@ -1,27 +1,29 @@
-const Localstartegy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
-function initialize(passport, getUserByEmail)  { 
-    const authenticateUser = (email,password,done)=>{
-        const user = getUserByEmail(email);
-        if(user == null){
-            return done(null,false,{message:'No user with that email'});
-        }
-
-        try {
-            if(await bcrypt.compare(password,user.password)){
-                return done(null,user);
-            }
-            else{
-                return done(null,false,{message:'Password incorrect'});
-            }
-        } catch (e) {
-            return done(e);
-        }
-
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email)
+    if (user == null) {
+      return done(null, false, { message: 'No user with that email' })
     }
-   passport.use(new Localstartegy({usernameField : 'email'}, authenticateUser));
-    passport.serializeUser(serializeUser);
-    passport.deserializeUser(deserializeUser);
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'Password incorrect' })
+      }
+    } catch (e) {
+      return done(e)
+    }
+  }
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
 }
-module.exports = initialize;
+
+module.exports = initialize
